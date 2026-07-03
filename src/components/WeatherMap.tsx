2702a899381820cbdabac5281e719cef2f2ecd72
miccowang66 <miccowang66@users.noreply.github.com>
2026-07-03 12:53:02 +0800

    Upgrade map tiles: CartoDB Dark Matter (Windy-like modern dark style)

--- src/components/WeatherMap.tsx
+++ src/components/WeatherMap.tsx
@@ -64,9 +64,10 @@ export default function WeatherMap({
       zoomControl: true,
     })
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 18,
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 19,
     }).addTo(map)
 
     const markersLayer = L.layerGroup().addTo(map)
